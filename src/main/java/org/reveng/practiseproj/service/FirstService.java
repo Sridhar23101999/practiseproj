1e42282cd8e1b986c2e9e0b6066c5a804334062a
sridhar <sridhar1215225@gmail.com>
2025-10-26 00:36:28 +0530

    commit service

--- src/main/java/org/reveng/practiseproj/service/FirstService.java
+++ src/main/java/org/reveng/practiseproj/service/FirstService.java
@@ -1,5 +1,8 @@
 package org.reveng.practiseproj.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.reveng.practiseproj.entity.FirstEntity;
 import org.reveng.practiseproj.repository.FirstRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +20,11 @@ public class FirstService {
 
     public String testDB(){
         FirstEntity firstEntity = new FirstEntity();
+        List<FirstEntity> repo = new ArrayList<>();
         firstEntity.setId(1);
         firstEntity.setName("Sri");
         firstRepository.save(firstEntity);
-        System.out.println(firstRepository.findAll());
+        repo = firstRepository.findAll();
         return "Success";
     }
 }
