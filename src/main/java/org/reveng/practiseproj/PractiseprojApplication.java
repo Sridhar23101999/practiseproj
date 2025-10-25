2b1a712a67fd5a42438fb6399dd113270363a7f0
sridhar <sridhar1215225@gmail.com>
2025-10-25 13:22:44 +0530

    creating new project

---
+++ src/main/java/org/reveng/practiseproj/PractiseprojApplication.java
@@ -0,0 +1,13 @@
+package org.reveng.practiseproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PractiseprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PractiseprojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/reveng/practiseproj/PractiseprojApplicationTests.java
@@ -0,0 +1,13 @@
+package org.reveng.practiseproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractiseprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
