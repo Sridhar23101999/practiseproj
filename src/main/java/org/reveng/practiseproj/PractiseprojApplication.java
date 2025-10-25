cb6a314568ba22a0f3b296c0f2b03572bc988820
sridhar <sridhar1215225@gmail.com>
2025-10-26 00:18:46 +0530

    removing some configs.

--- src/main/java/org/reveng/practiseproj/PractiseprojApplication.java
+++ src/main/java/org/reveng/practiseproj/PractiseprojApplication.java
@@ -2,12 +2,8 @@ package org.reveng.practiseproj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan("org.reveng.practiseproj.entity")
-@EnableJpaRepositories("org.reveng.practiseproj.repository")
 public class PractiseprojApplication {
 
 	public static void main(String[] args) {
